0ad8927fb30cfe0a6b6b71433b6a23b309388477
neemoh <nima.en@gmail.com>
2017-04-17 17:52:21 -0700

    copy AcOverlay class into new class to remove old tasks

---
+++ src/ac_overlay/OverlayROSConfig.cpp
@@ -0,0 +1,5 @@
+//
+// Created by charm on 4/17/17.
+//
+
+#include "OverlayROSConfig.h"
---
+++ src/ac_overlay/OverlayROSConfig.h
@@ -0,0 +1,16 @@
+//
+// Created by charm on 4/17/17.
+//
+
+#ifndef TELEOP_VISION_OVERLAYROSCONFIG_H
+#define TELEOP_VISION_OVERLAYROSCONFIG_H
+
+
+
+class OverlayROSConfig {
+
+};
+
+
+
+#endif //TELEOP_VISION_OVERLAYROSCONFIG_H
