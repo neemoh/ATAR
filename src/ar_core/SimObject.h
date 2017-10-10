71bdd95a6521310768f0ab40d30a90e9b4a3ccf6
neemoh <nima.en@gmail.com>
2017-10-10 20:22:33 +0200

    improve comments and description

--- src/ar_core/SimObject.h
+++ src/ar_core/SimObject.h
@@ -12,6 +12,7 @@
 /**
  * \class SimObject
  * \brief This class represents a simulated object with graphics and physics.
+ *
  * When a SimObject is generated its graphic actor (actor_) and physics body
  * (rigid_body_) has to be added to the renderer and physics world.
  * After which, the pose of the graphical representation will be
@@ -19,16 +20,48 @@
  * toBulletVTKMotionState member. To learn more about how to add simObjects
  * to the simulation world refer to VTKTask.h
  *
- * To construct a SimObject at least 5 arguments are needed.
+ * \arg To construct a SimObject at least 3 arguments are needed:
  *
- * To get static objects make a dynamic one with zero mass.
+ *      ObjectShape: Can be predefined shapes (ex: ObjectType::SPHERE) or mesh
+ *      (ObjectType::MESH). In the Mesh case the address of the .obj file
+ *      must be provided with the mesh_address argument.
  *
- * Meshes are decomposed into approximated compound meshes.
+ *      dimensions: If ObjectShape is one of the predefined shapes the
+ *      dimensions must be specified as an array described in ObjectShape
+ *      enum definition.
  *
- * IMPORTANT NOTE: We were interested in objects with dimensions in the
- * order of a few millimiters. It turned out that the bullet simulation
- * becomes unstable for such small dimensions. To get around this, the
- * dimensions of all the bullet related things are multiplied by B_DIM_SCALE.
+ *      ObjectType: Four cases
+ *          1. NOPHYSICS: The physics will not be initialized.
+ *          2. KINEMATIC: The pose of the object will be set at each
+ *          iteration using the SetKinematicPose. This is useful for things
+ *          like tools that their pose is read from a master device.
+ *          3. DYNAMIC and density!=0.0 The pose of the object will be
+ *          automatically update from the physics simulation.
+ *          4. DYNAMIC and no density provided (density defults to 0.0) in
+ *          this case the object will be Static, meaning its pose will be
+ *          constant but dynamic objects can collide with the object
+ * Other params:
+ *      Density: Is needed for dynamic objects to calculate their mass.
+ *      pose: The Initial pose of the dynamics objects.
+ *      friction: friction!
+ *      id: can be used in collision detection
+ *
+ * \attention MESH:  Meshes are decomposed into approximated compound meshes
+ * using the VHACD method. Generating the approximated compound mesh can take
+ * up to a few minutes. So we do this only once and save the compound mesh in
+ * a separate file that has the same name of the original mesh file with an
+ * added _hacd. Next time the application is executed we search for the file
+ * with _hacd and if found, it is used and compound mesh generation is not
+ * repeated.
+ *
+ * \attention Dimension scaling: We were interested in objects with
+ * dimensions in the order of a few millimiters. It turned out that the bullet
+ * simulation becomes unstable for such small dimensions. To get around this,
+ * the dimensions of all the bullet related things are multiplied by
+ * B_DIM_SCALE.
+ *
+ * \author Nima Enayati
+ * \date June 2017
  */
 
 
@@ -55,23 +88,43 @@ enum ObjectShape {
 class SimObject {
 
 public:
+    /**
+    * SimObject constructor.
+    * Needs at least 3 arguments.
+    */
     SimObject(const ObjectShape shape, const ObjectType type,
               const std::vector<double> dimensions,
               const KDL::Frame &pose=KDL::Frame(),
               const double density=0.0, const double friction = 0.1,
-              const std::string data = {}, const int id = 0);
+              const std::string mesh_address = {}, const int id = 0);
 
     ~SimObject();
 
+    /**
+    * Returns the rigid body member of the object.
+    */
     btRigidBody* GetBody() { return rigid_body_; }
 
+    /**
+    * Returns the VTK actor member of the object.
+    */
     vtkSmartPointer<vtkActor> GetActor() { return actor_; };
 
+    /**
+    * Set the pose if the object is kinematic.
+    */
     void SetKinematicPose(double pose[]);
 
+    /**
+    * Can be used in physics collision handling
+    */
     int GetId() {return id_; };
 
+    /**
+    * Get the pose of the object.
+    */
     KDL::Frame GetPose();
+
 private:
 
     int id_;
@@ -95,7 +148,9 @@ vtkSmartPointer<vtkMatrix4x4> KDLFrameToVTKMatrix(const KDL::Frame &pose);
 //KDL::Frame VTKMatrixToKDLFrame(const vtkSmartPointer<vtkMatrix4x4>);
 
 // -----------------------------------------------------------------------------
-// callback for pairwise collision test
+/**
+* callback for pairwise collision test.
+*/
 struct MyContactResultCallback : public btCollisionWorld::ContactResultCallback
 {
     bool connected;
