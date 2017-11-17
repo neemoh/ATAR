e542ca58ab65ffa494d45aba23793c7b9a0f3720
neemoh <nima.en@gmail.com>
2017-11-17 11:10:17 +0100

    add the cubes

--- src/ar_core/tasks/TaskDemo3.cpp
+++ src/ar_core/tasks/TaskDemo3.cpp
@@ -6,8 +6,8 @@
 
 TaskDemo3::TaskDemo3()
 {
-    // the only needed argument to construct a Renderer is the nodehandle ptr
-    // The rest have default values.
+    // create a rendering object with desired parameters. In particular, we
+    // want to set the ar_mode flag to true
     graphics = std::make_unique<Rendering>(
             /*view_resolution=*/std::vector<int>({920, 640}),
             /*ar_mode=*/true,
@@ -16,32 +16,45 @@ TaskDemo3::TaskDemo3()
             /*borders_off=*/false,
             /*window_positions=*/std::vector<int>({700,50}));
 
+    ros::NodeHandle n("~");
     // -------------------------------------------------------------------------
-    // Create 1 simple dynamic cube (pointer local in the constructor)
-    SimObject *cube;
-    {
+    // Create 4 cubes with no dynamics and put them on 4 squares of the
+    // charuco board
+
+    // get the length of the board squares
+    std::vector<float> board_params = std::vector<float>(5, 0.0);
+    n.getParam("board_params", board_params);
+    double distance = board_params[3];
+
+    // create the positions of the 4 cubes
+    double positions[4][2] = {
+            {distance/2,                distance/2 },
+            {5*distance + distance/2,   distance/2 },
+            {5*distance + distance/2,   3*distance +distance/2 },
+            {distance/2,                3*distance +distance/2 },
+      };
+
+    SimObject *cube[4];
+
+    for(int i = 0; i < 4; ++i) {
+
         // define object dimensions
-        std::vector<double> dimensions = {0.02, 0.01, 0.005};
+        std::vector<double> dimensions = {distance, distance, 0.05};
 
         // define object Pose
-        KDL::Frame pose(KDL::Rotation::Quaternion( 0., 0., 0., 1.)
-                , KDL::Vector(0.07, 0.055, 0.05 ));
+        KDL::Frame pose(KDL::Rotation::Quaternion(0., 0., 0., 1.),
+                        KDL::Vector(positions[i][0], positions[i][1], 0.0));
 
         // construct the object
-        cube = new SimObject(ObjectShape::BOX, ObjectType::DYNAMIC,
-                             dimensions);
+        cube[i] = new SimObject(ObjectShape::BOX, ObjectType::NOPHYSICS,
+                             dimensions,pose);
 
         // we can access all the properties of a VTK actor
-        cube->GetActor()->GetProperty()->SetColor(colors.OrangeRed);
+        cube[i]->GetActor()->GetProperty()->SetColor(colors.Red);
 
         // we need to add the SimObject to the task. Without this step
         // the object is not going to be used
-        AddSimObjectToTask(cube);
+        AddSimObjectToTask(cube[i]);
     }
-
-//    throw std::runtime_error("Yo");
-}
-
-void TaskDemo3::TaskLoop() {
 
 }
--- src/ar_core/tasks/TaskDemo3.h
+++ src/ar_core/tasks/TaskDemo3.h
@@ -5,15 +5,19 @@
 #ifndef ATAR_TASKDEMO3_H
 #define ATAR_TASKDEMO3_H
 
+// This demo shows how to create ar tasks in which 3D objects are overlaid on
+// the images of a real camera. Print the charuco_d0_h4_w6_sl200_m10_ml150
+// file on a paper and measure the length of the black squares and the marker
+// squares with a ruler. Update the last two elements of the board_params
+// parameter of the test_ar.launch file with these measurements. When you run
+// the task 4 virtual cubes should be placed on 4 black squares of the board
+// when the board is in the field of the view of the camera.
 
 #include <src/ar_core/SimTask.h>
 
 class TaskDemo3: public SimTask {
 public:
     explicit TaskDemo3();
-
-    void TaskLoop() override;
-
 };
 
 
